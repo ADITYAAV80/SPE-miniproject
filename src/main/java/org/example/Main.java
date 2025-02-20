c75d13995919b66460a9136642f79020ca4c355b
aditya <aditya@Ubuntu-ROS.myguest.virtualbox.org>
2025-02-20 17:43:44 +0530

    MiniProject

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        int result = calculator.add(5, 3);
+        System.out.println("5 + 3 = " + result);
+
+    }
+}
